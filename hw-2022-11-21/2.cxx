c3f84b420548783609633ab6175980972ecfd657
Tiko7454 <tiko7454av@gmail.com>
2022-11-16 02:25:46 +0400

    added checks and messages

--- hw-2022-11-21/2.cxx
+++ hw-2022-11-21/2.cxx
@@ -6,7 +6,13 @@
 int main() {
     pid_t pid = fork();
     if (pid == 0) {
-        kill(getppid(), SIGKILL);
+        if (kill(getppid(), SIGKILL) != 0) {
+            std::cout << "couldn't kill:(" << std::endl;
+        } else {
+            sleep(1);
+            std::cout << "yay, done" << std::endl;
+            std::cout << "new father is " << getppid() << std::endl;
+        }
     } else {
         wait(nullptr);
     }
