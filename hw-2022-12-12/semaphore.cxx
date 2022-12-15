d78ef43b6d81578f196e3d21ccb85d9548666691
tiko7454 <tiko7454av@gmail.com>
2022-12-15 18:34:33 +0400

    fixed thread errors

--- hw-2022-12-12/semaphore.cxx
+++ hw-2022-12-12/semaphore.cxx
@@ -37,8 +37,8 @@ public:
 };
 
 
+Integer integer{0};
 void* increment(void* arg) {
-    Integer* integer = static_cast<Integer*>(arg);
     const std::size_t count = 100000;
     for (std::size_t i{}; i < count; i++){
        integer++;
@@ -49,38 +49,32 @@ void* increment(void* arg) {
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
     std::size_t thread_count = 100;
-    Integer* integer = new Integer{0};
-    std::vector<pthread_t*> threads(thread_count, new pthread_t{});
+    std::vector<pthread_t> threads(thread_count);
 
-    for (std::size_t i = 0; i < thread_count; i++){
+    for (std::size_t i = 0; i < thread_count; i++) {
         if (pthread_create(
-                threads[i],
+                &threads[i],
                 nullptr,
                 increment,
-                static_cast<void*>(integer)) != 0
-        ){
+                nullptr
+            ) != 0) {
             return 1;
         }
     }
 
-    for (std::size_t i = 0; i < thread_count; i++){
-        if (pthread_join(*threads[i], nullptr) != 0){
+    for (std::size_t i = 0; i < thread_count; i++) {
+        if (pthread_join(threads[i], nullptr) != 0){
             return 2;
         }
     }
-    std::cout << integer->get_value() << std::endl;
-
-    delete integer;
-    for (auto thread : threads) {
-        delete thread;
-    }
+    std::cout << integer.get_value() << std::endl;
 
     auto stop = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
         stop - start
     );
 
     std::ofstream time_file{"time.txt", std::ios_base::app};
-    time_file << "semaphore: " << duration.count() << " seconds" << std::endl;
+    time_file << "semaphore: " << duration.count() << " milliseconds" << std::endl;
 }
