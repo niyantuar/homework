228d9caee386a91c34688f79d65da67c34135a32
niyantuar <115151811+niyantuar@users.noreply.github.com>
2022-12-19 11:08:25 +0400

    Update mutex.cxx

--- hw-2022-12-12/mutex.cxx
+++ hw-2022-12-12/mutex.cxx
@@ -1,79 +1,95 @@
-#include <iostream>
+#ifndef TASK_QUEUE_HXX
+#define TASK_QUEUE_HXX
+
 #include <cstddef>
-#include <vector>
-#include <fstream>
-#include <chrono>
+#include <queue>
+#include <functional>
+#include <signal.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 
-class Integer {
-  private:
-    int _value;
-    pthread_mutex_t* _mutex;
+class TaskQueue {
   public:
-    Integer(int value): _value{value}, _mutex(new pthread_mutex_t{}) {
+    TaskQueue(std::size_t thread_count) 
+      : _thread_count{thread_count}
+      , _threads(thread_count)
+      , _semaphore{new sem_t{}}
+      , _mutex{new pthread_mutex_t{}} {
+        sem_init(_semaphore, 0, thread_count);
         pthread_mutex_init(_mutex, nullptr);
-    }   
-    
-    int get_value() const {
-        return _value;
+        for (std::size_t i{}; i < thread_count; ++i) {
+          _free_threads.push(i);
+        }
     }
 
-    void set_value(int value) {
-        _value = value;
+    using function_t = std::function<void*(void*)>;
+    using arg_t = void*;
+    using task_t = std::pair<function_t, arg_t>;
+
+    void push_task(function_t function, arg_t arg, void** output_arg) {
+        _tasks.emplace(function, arg);
+        _output_arguments.push(output_arg);
     }
 
-    void operator++(int) {
+    void run() {
+      while (_tasks.empty()) {
+          thread_create(
+              _get_a_free_thread(),
+              nullptr,
+              _do_a_task,
+              nullptr
+          );
+      }
+    }
+
+    ~TaskQueue() {
+      sem_destroy(_semaphore);
+      pthread_mutex_destroy(_mutex);
+      delete _semaphore;
+    }
+  private:
+    std::pair<task_t, void**> _get_a_new_task() {
         pthread_mutex_lock(_mutex);
-        _value++;
+        auto task = _tasks.front();
+        _tasks.pop();
+        auto output_argument = _output_arguments.front();
+        _output_arguments.pop();
         pthread_mutex_unlock(_mutex);
+        return {task, output_argument};
     }
 
-    ~Integer() {
-        pthread_mutex_destroy(_mutex);
-        delete _mutex;
+    pthread_t* _get_a_free_thread() {
+        pthread_mutex_lock(_mutex);
+        std::size_t free_thread_index = _free_thread_indexes.front();
+        _free_thread_indexes.pop();
+        auto ret = &_threads[free_thread_index];
+        pthread_mutex_unlock(_mutex);
+        return ret;
     }
+
+    void* _do_a_task(void*) {
+      sem_wait(_semaphore);
+      auto [task, output_arg] = _get_a_new_task();
+      thread_create(
+          _get_a_free_thread();
+          nullptr,
+          task.first,
+          task.second
+      );
+      sem_post(_semaphore);
+      
+      return ret;
+    }
+  private:
+    std::queue<task_t> _tasks;
+    std::queue<void**> _argument_arguments;
+    std::vector<pthread_t> _threads;
+    sem_t* _semaphore;
+    pthread_mutex_t* _mutex;
+    std::size_t _thread_count;
+    std::queue<std::size_t> _free_thread_indexes;
 };
 
 
-Integer integer{0};
-void* increment(void* arg) {
-    const std::size_t count = 100000;
-    for (std::size_t i{}; i < count; i++){
-       integer++;
-    }
-    return nullptr;
-}
-
-int main() {
-    auto start = std::chrono::high_resolution_clock::now();
-    std::size_t thread_count = 100;
-    std::vector<pthread_t> threads(thread_count);
-    
-    for (std::size_t i = 0; i < thread_count; i++) {
-        if (pthread_create(
-                &threads[i],
-                nullptr,
-                increment,
-                nullptr
-            ) != 0) {
-            return 1;
-        }
-    }
-    
-    for (std::size_t i = 0; i < thread_count; i++) {
-        if (pthread_join(threads[i], nullptr) != 0){
-            return 2;
-        }
-    }
-    std::cout << integer.get_value() << std::endl;
-    
-    auto stop = std::chrono::high_resolution_clock::now();
-
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-        stop - start
-    );
-
-    std::ofstream time_file{"time.txt", std::ios_base::app};
-    time_file << "mutex: " << duration.count() << " milliseconds" << std::endl;
-}
+#endif // TASK_QUEUE_HXX
