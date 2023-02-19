5c44e5d45b3a51a1d1509e893c312f83f7c0a631
Tiko7454 <tiko7454av@gmail.com>
2023-02-19 16:45:43 +0400

    fixed a bug when exit wasn't performed when elements where <=50

--- hw-2023-02-23/main.cxx
+++ hw-2023-02-23/main.cxx
@@ -3,9 +3,11 @@
 
 
 pthread_mutex_t mutex;
+pthread_mutex_t mutex_exit;
 pthread_mutex_t mutex_ostream;
 pthread_cond_t cond;
 std::size_t count;
+bool is_time_to_exit = false;
 
 void atomic_println(const std::string& str) {
     pthread_mutex_lock(&mutex_ostream);
@@ -17,10 +19,20 @@ void* f(void*) {
     pthread_mutex_lock(&mutex);
     while (count <= 10) {
         pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_lock(&mutex_exit);
+        if (is_time_to_exit) {
+            return nullptr;
+        }
+        pthread_mutex_unlock(&mutex_exit);
     }
     atomic_println(">10");
     while (count <= 50) {
         pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_lock(&mutex_exit);
+        if (is_time_to_exit) {
+            return nullptr;
+        }
+        pthread_mutex_unlock(&mutex_exit);
     }
     atomic_println(">50");
     pthread_mutex_unlock(&mutex);
@@ -41,7 +53,10 @@ int main() {
         pthread_mutex_unlock(&mutex);
     }
     atomic_println(std::to_string(count));
-
+    pthread_mutex_lock(&mutex_exit);
+    is_time_to_exit = true;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex_exit);
     pthread_cond_destroy(&cond);
     pthread_mutex_destroy(&mutex);
 }
