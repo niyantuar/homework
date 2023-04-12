7806ba51eb8f41a99522d53cb525441a73ad5800
Tiko7454 <tiko7454av@gmail.com>
2023-04-12 23:11:21 +0400

    added send functionality

--- hw-2023-04-13/client.cxx
+++ hw-2023-04-13/client.cxx
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+
+
+struct SocketAddress {
+    sa_family_t sa_family;
+    char sa_data[1024];
+} socket_address;
+
+int client_fd{};
 
 void* send(void*) {
     std::string receiver_name;
     std::string message;
     std::cin >> receiver_name >> message;
-
+    sockaddr_un address{};
+    address.sun_family = AF_LOCAL;
+    strcpy(address.sun_path, receiver_name.c_str());
+    sendto(
+        client_fd,
+        message.c_str(),
+        message.size() + 1,
+        0,
+        reinterpret_cast<sockaddr*>(&address),
+        sizeof(address)
+    );
+    return nullptr;
 }
 
 void* receive(void*) {
@@ -21,7 +44,24 @@ int main(int argc, char** argv) {
     }
     std::string client_name{argv[1]};
 
-    // create socket
+    client_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if (client_fd == -1) {
+        perror("socket error");
+        return 1;
+    }
+
+    socket_address.sa_family = AF_LOCAL;
+    std::strcpy(socket_address.sa_data, client_name.c_str());
+    if (
+        bind(
+            client_fd,
+            reinterpret_cast<sockaddr*>(&socket_address),
+            sizeof(socket_address)
+        ) == -1
+    ) {
+        perror("bind error");
+        return 1;
+    }
 
     pthread_t sender{};
     pthread_create(&sender, nullptr, send, nullptr);
